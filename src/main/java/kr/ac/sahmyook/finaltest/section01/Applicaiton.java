1a9d3f52293aa63cf63f3eab1c6efff62a507d0a
sejin0211 <sejin0211@naver.com>
2023-05-11 15:35:06 +0900

    init setting

---
+++ src/main/java/kr/ac/sahmyook/finaltest/section01/Applicaiton.java
@@ -0,0 +1,7 @@
+package kr.ac.sahmyook.finaltest.section01;
+
+public class Applicaiton {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/kr/ac/sahmyook/finaltest/section01/Calculator.java
@@ -0,0 +1,4 @@
+package kr.ac.sahmyook.finaltest.section01;
+
+public class Calculator {
+}
